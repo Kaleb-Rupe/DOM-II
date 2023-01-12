a2b4390373b52b6d5773b47498963e6780a961cf
Kaleb-Rupe <57547999+Kaleb-Rupe@users.noreply.github.com>
2023-01-12 00:22:41 -0500

    wip pretty much complete but I wasnt sure what to do with the preventDefault

--- src/index.js
+++ src/index.js
@@ -1,3 +1,60 @@
-import './less/index.less'
+import "./less/index.less";
 
 // Your code goes here!
+
+window.onload = function (evt) {
+  console.log(`event ${evt.type} fired! Ready to go!`);
+  const heading = document.querySelector("h1");
+  heading.textContent = "READY TO GO!!";
+
+  window.addEventListener("copy", () => {
+    navigator.clipboard.readText().then((text) => {
+      heading.textContent += text;
+    });
+  });
+
+  document.body.addEventListener("click", (evt) => {
+    evt.target.classList.toggle("mirror");
+  });
+
+  document.body.addEventListener("dbclick", (evt) => {
+    evt.target.innerHTML = "";
+    evt.preventDefault();
+  });
+
+  window.addEventListener("keydown", (evt) => {
+    if (evt.key == 6) {
+      document.body.innerHTML = "<h1>YOU RAN ORDER 66!</h1>";
+    }
+  });
+
+  document.body.addEventListener("mousemove", (evt) => {
+    const { clientX, clientY } = evt;
+    // console.log(`mouse is at ${clientX}, ${clientY} `);
+  });
+
+  const destinations = document.querySelectorAll(".destination");
+
+  for (let destination of destinations) {
+    destination.addEventListener("mouseenter", () => {
+      destination.style.fontWeight = "bold";
+    });
+    destination.addEventListener("mouseleave", () => {
+      setTimeout(() => {
+        destination.style.fontWeight = "initial";
+      });
+    });
+  }
+
+  const navLink = document.querySelector(".nav-container");
+
+  navLink.addEventListener("mouseenter", (event) => {
+    event.target.style.backgroundColor = "Tan";
+    event.target.style.borderColor = "Red";
+  });
+
+  navLink.addEventListener("mouseleave", (event) => {
+    event.target.style.backgroundColor = "initial";
+    event.target.style.borderColor = "initial";
+  });
+};
